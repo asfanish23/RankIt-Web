69bd01720147f6fe8c9872e9252b4bceb1dcc931
Asfa Danish Bin Hezri <ADHezri@gmail.com>
2026-06-26 17:51:14 +0800

    Implement category CRUD with Laravel API integration

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,9 @@ Route::middleware('auth:sanctum')->group(function () {
 });
 
 Route::get('/categories', [CategoryController::class, 'index']);
+Route::post('/categories', [CategoryController::class, 'store']);
+Route::put('/categories/{id}', [CategoryController::class, 'update']);
+Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
 
 Route::get('/topics', [TopicController::class, 'index']);
 
